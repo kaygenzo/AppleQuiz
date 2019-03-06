bfccc78b664ea3f2d1013ca5942a3a019deab19f
Karim Yarboua <karim.yarboua@open-groupe.com>
2019-03-06 11:14:20 +0100

    Initial Commit

---
+++ AppleQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleQuiz
+//
+//  Created by Karim Yarboua on 06/03/2019.
+//  Copyright © 2019 Karim Yarboua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
